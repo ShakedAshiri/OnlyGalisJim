fa4af9a5cf7eadb91c1ac1ce6254500efabf0f7b
shakeda <shaked.ashiri@modelitymp.com>
2022-02-05 15:09:39 +0200

    start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LevelPageComponent} from "./level-page/level-page.component";
+import {MainPageComponent} from "./main-page/main-page.component";
+import {EndingComponent} from "./ending/ending.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'end/:id', component: EndingComponent },
+  { path: 'level/:id', component: LevelPageComponent }]
+;
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { LevelPageComponent } from './level-page/level-page.component';
+import { EndingComponent } from './ending/ending.component';
+import { Level1Component } from './level1/level1.component';
+import { Level2Component } from './level2/level2.component';
+import { Level3Component } from './level3/level3.component';
+import { Level4Component } from './level4/level4.component';
+import { Level5Component } from './level5/level5.component';
+import { Level6Component } from './level6/level6.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    LevelPageComponent,
+    EndingComponent,
+    Level1Component,
+    Level2Component,
+    Level3Component,
+    Level4Component,
+    Level5Component,
+    Level6Component
   ],
   imports: [
     BrowserModule,
